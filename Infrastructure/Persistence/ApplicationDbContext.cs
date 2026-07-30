88766a560a970586ded08e741f2a11e833d70c97
nawaraaaj <nawarajneupane2003@gmail.com>
2026-07-30 12:42:39 +0545

    feat: add global query filter to exclude soft-deleted entities

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Infrastructure.Persistence;
 
@@ -10,4 +11,24 @@ public class ApplicationDbContext : DbContext
     public DbSet<StatusHistory> StatusHistories { get; set; }
    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                var parameter = Expression.Parameter(entityType.ClrType, "e");
+                var property = Expression.Call(
+                    typeof(EF), nameof(EF.Property), new[] { typeof(bool) },
+                    parameter, Expression.Constant(nameof(BaseEntity.IsDeleted)));
+                var condition = Expression.Equal(property, Expression.Constant(false));
+                var lambda = Expression.Lambda(condition, parameter);
+
+                modelBuilder.Entity(entityType.ClrType).HasQueryFilter(lambda);
+            }
+        }
+    }
 }
